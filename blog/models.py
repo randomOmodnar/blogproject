23b988f3c99e16679d9daba25834b90f65cf1f43
randomOmodnar <53683707+randomOmodnar@users.noreply.github.com>
2020-08-11 10:25:16 +0800

    创建 Django 博客的数据库模型

--- blog/models.py
+++ blog/models.py
@@ -6,21 +6,28 @@ class Category(models.Model):
     """分类"""
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 class Tag(models.Model):
     """标签"""
     name =models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 class Post(models.Model):
     """文章数据库表"""
     title = models.CharField(max_length=70)
     body = models.TextField()
-    create_time = models.DateTimeField()
+    created_time = models.DateTimeField()
     modified_time = models.DateTimeField()
     excerpt = models.CharField(max_length=200,blank=True)
     category = models.ForeignKey(Category,on_delete=models.CASCADE)
     tags = models.ManyToManyField(Tag, blank=True)
     author = models.ForeignKey(User,on_delete=models.CASCADE)
 
-    
+    def __str__(self):
+        return self.name
 
     
