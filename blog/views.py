0195e5af9a413051ca5af4fa7200d9c0b8507bb1
randomOmodnar <53683707+randomOmodnar@users.noreply.github.com>
2020-08-11 10:43:58 +0800

    Django 的接客之道

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+
+# def index(request):
+#     return HttpResponse('welcome to my blog!')
+
+def index(request):
+    return render(request,'blog/index.html',context={
+        'title':'Home page of my blog',
+        'welcome':'Welcome to my blog',
+    })
